957d2214b53c1ce961472f7d2412461a332d7245
asoritau <asoritau@sdl.com>
2021-05-10 11:18:31 +0300

    Changed code to display html document

--- CatalogOnline/src/main/java/org/gr3/CatalogOnline/CatalogOnlineApplication.java
+++ CatalogOnline/src/main/java/org/gr3/CatalogOnline/CatalogOnlineApplication.java
@@ -2,11 +2,13 @@ package org.gr3.CatalogOnline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class CatalogOnlineApplication {
 
 	public static void main(String[] args) {
