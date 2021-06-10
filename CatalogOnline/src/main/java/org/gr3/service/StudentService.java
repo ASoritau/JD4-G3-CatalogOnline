22bb9dad196e934a3e912fc536fb0d233a147a0f
asoritau <asoritau@sdl.com>
2021-06-10 23:58:24 +0300

    Added method to get all students in StudentService.java

--- CatalogOnline/src/main/java/org/gr3/service/StudentService.java
+++ CatalogOnline/src/main/java/org/gr3/service/StudentService.java
@@ -1,4 +1,30 @@
 package org.gr3.service;
 
+import org.gr3.model.Student;
+import org.gr3.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    UserService userService;
+
+    public List<Student> getAllStuents() {
+        List<User> users = userService.getAllUsers();
+
+        List<Student> studentsList = new ArrayList<>();
+
+        for (User user : users) {
+            if (user instanceof Student) {
+                studentsList.add((Student) user);
+            }
+        }
+
+        return studentsList;
+    }
 }
