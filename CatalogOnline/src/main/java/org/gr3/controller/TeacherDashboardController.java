13a5719fad9d68e71e92f23a2dfebc65bebef9fb
Noris Gavrilas <ngavrilas@sdl.com>
2021-06-13 16:33:10 +0300

    Updated TeacherDashboardController and teacherDashboard.html

--- CatalogOnline/src/main/java/org/gr3/controller/TeacherDashboardController.java
+++ CatalogOnline/src/main/java/org/gr3/controller/TeacherDashboardController.java
@@ -1,12 +1,7 @@
 package org.gr3.controller;
 
-import org.gr3.model.Absence;
-import org.gr3.model.Grade;
-import org.gr3.model.Student;
-import org.gr3.model.Teacher;
-import org.gr3.service.AbsenceService;
-import org.gr3.service.GradeService;
-import org.gr3.service.StudentService;
+import org.gr3.model.*;
+import org.gr3.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SessionAttributes("teacher")
 @Controller
@@ -32,6 +28,9 @@ public class TeacherDashboardController {
     @Autowired
     AbsenceService absenceService;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/teacherDashboard")
     public String myDashboardPage(@ModelAttribute("teacher") Teacher teacher, RedirectAttributes redirectAttributes, BindingResult errors, Model model) {
         model.addAttribute("teacher", teacher);
@@ -39,6 +38,29 @@ public class TeacherDashboardController {
         List<Grade> grades = gradeService.getAllGrades();
         redirectAttributes.addFlashAttribute("teacher", teacher);
         redirectAttributes.addAttribute("grades", grades);
+
+        String teacherSubject = teacher.getSubject();
+
+        Student student_RaduAndreescu = (Student) userService.findByFirstNameAndLastName("Radu", "Andreescu");
+        List<Grade> grades_RaduAndreescu = gradeService.getStudentGradesForSubject((int) student_RaduAndreescu.getUserId(), teacherSubject);
+
+        Student student_AlinPascu = (Student) userService.findByFirstNameAndLastName("Alin", "Pascu");
+        List<Grade> grades_AlinPascu = gradeService.getStudentGradesForSubject((int) student_AlinPascu.getUserId(), teacherSubject);
+
+        Student student_RaduPop = (Student) userService.findByFirstNameAndLastName("Radu", "Pop");
+        List<Grade> grades_RaduPop = gradeService.getStudentGradesForSubject((int) student_RaduPop.getUserId(), teacherSubject);
+
+        model.addAttribute("grades_RaduAndreescu", grades_RaduAndreescu);
+        model.addAttribute("grades_AlinPascu", grades_AlinPascu);
+        model.addAttribute("grades_RaduPop", grades_RaduPop);
+
+        List<Absence> absences_RaduAndreescu = absenceService.getStudentAbsencesForSubject((int) student_RaduAndreescu.getUserId(), teacherSubject);
+        List<Absence> absences_AlinPascu = absenceService.getStudentAbsencesForSubject((int) student_AlinPascu.getUserId(), teacherSubject);
+        List<Absence> absences_RaduPop = absenceService.getStudentAbsencesForSubject((int) student_RaduPop.getUserId(), teacherSubject);
+
+        model.addAttribute("absences_RaduAndreescu", absences_RaduAndreescu);
+        model.addAttribute("absences_AlinPascu", absences_AlinPascu);
+        model.addAttribute("absences_RaduPop", absences_RaduPop);
 
         return "TeacherDashboard";
     }
